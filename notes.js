05501fb3f8a40bfaa41f2c7c377d79c222cdb0b2
Evan Gao <gaoevan37@gmail.com>
2023-10-13 09:22:41 -0400

    notes

---
+++ notes.js
@@ -0,0 +1,15 @@
+const DOMSelectors = {
+    form: document.querySelector('#form'),
+    firstName: document.querySelector(".first-name"),
+    h2: document.querySelectorAll("h2") //needs query selector all
+};
+
+// console.log(DOMSelectors.firstName);
+
+DOMSelectors.form.addEventListener("submit", function(event){
+    event.preventDefault();
+    console.log(DOMSelectors.firstName.value);
+    DOMSelectors.h2.forEach(
+        (el) => el.textContent = DOMSelectors.firstName.value
+    );
+});
